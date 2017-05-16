c6a8f2d00fc6be908048499e61e18469e0bb9556
Lizette Romero <lmromero@anderson.edu>
2017-05-16 19:37:47 -0400

    add appending child

--- index.js
+++ index.js
@@ -12,12 +12,24 @@ renderListItem(value) {
   },
 
     renderList(person) {
-    const list = document.createElement('ul')
+   const list = document.createElement('ul')
     Array.from(person).map((input, _i, _elementsArray) => {
       if (input.value) {
+          var count = 0
         let value = input.value
         let li = this.renderListItem(value)
-        list.appendChild(li)
+        var textnode = document.createTextNode(value)
+    //    if (count === 0){
+       list.appendChild(textnode)
+    //     count++
+    //    }
+    //    else{
+        // list.insertBefore(li, list.childNodes[0]);
+     //  }
+        
+
+        
+       
       }
     })
 
@@ -29,7 +41,7 @@ renderListItem(value) {
     const form = ev.target
     const details = document.querySelector('.details')
     const list = this.renderList(form.elements)
-
+    form.reset()
    details.appendChild(list)
   },
 }
