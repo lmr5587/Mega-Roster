00e945aa77ef8641e76c60ba73c73049ae33844c
Lizette Romero <lmromero@anderson.edu>
2017-05-17 11:04:28 -0400

    create student object with ID

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 $(document).foundation()
+
+const megaroster = {
+    students:[],
+  init(listSelector) {
+      this.studentList=document.querySelector(listSelector)
+      this.max=0
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
+
+  addStudent(ev) {
+    ev.preventDefault()
+    const f=ev.target
+    const student = {
+        id: this.max + 1,
+        name:f.stidentName.value,
+    }
+    this.buildListItem(student)
+    this.max++
+  },
+  buildListItem(student){
+
+  }
+}
+megaroster.init('#studentList')
